b0e568182ccf89c7ee259309a308a311f182e9db
asdadsdasd <mkozarez12345@mail.ru>
2024-11-08 23:38:08 +0300

    init project

---
+++ src/TextDataReader.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from Types import DataType
+from DataReader import DataReader
+
+class TextDataReader(DataReader):
+    def __init__(self):
+        self.key = ""
+        self.students: DataType = {}
+
+    def read(self, path):
+        with open(path, encoding='utf-8') as file:
+            for line in file:
+                if not line.startswith(" "):
+                    self.key = line.strip()
+                    self.students[self.key] = []
+                else:
+                    subj, score = line.split(":", maxsplit=1)
+                    self.students[self.key].append(
+                        (subj.strip(), int(score.strip()))
+                    )
+        return self.students
